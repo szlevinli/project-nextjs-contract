293d217fea06b6f38ecc9b4f4107e4e9d330e55b
levin li <3259963@qq.com>
2021-05-25 11:31:06 +0800

    删除不必要的注释代码

--- lib/utils/withFetcher.ts
+++ lib/utils/withFetcher.ts
@@ -1,26 +1,6 @@
-import React from 'react';
-import useSWR, { SWRResponse } from 'swr';
-import { io as IO, ioEither as IE, either as E } from 'fp-ts';
+import { either as E, io as IO, ioEither as IE } from 'fp-ts';
 import { pipe } from 'fp-ts/function';
-
-export const withFetcher =
-  <T>(apiUrl: string) =>
-  (fetcher: (url: string) => Promise<T>) =>
-  (Component: React.FC<Record<string, unknown> & { data: T }>) =>
-  (props: Record<string, unknown>) => {
-    const { data, error } = useSWR<T>(apiUrl, fetcher);
-
-    if (error) return React.createElement('div', null, `Error: ${error}`);
-    if (!data) return React.createElement('div', null, 'loading...');
-
-    return React.createElement(Component, { ...props, data });
-  };
-
-// const getDataFromApi =
-//   <Data, Error>(apiUrl: string) =>
-//   (fetcher: (url: string) => Promise<Data>): IO.IO<SWRResponse<Data, Error>> =>
-//   () =>
-//     useSWR<Data>(apiUrl, fetcher);
+import useSWR, { SWRResponse } from 'swr';
 
 export const ioSwr =
   <Data, Error>(apiUrl: string) =>
@@ -41,18 +21,3 @@ export const ioEitherSwr = <Data, Error>(
         : E.right(E.left(undefined))
     )
   );
-
-// export const kkk =
-//   <Data, Error>(fa: IO.IO<SWRResponse<Data, Error>>) =>
-//   (Component: React.FC<Record<string, unknown> & { data: Data }>) =>
-//   (props: Record<string, unknown>): IE.IOEither<JSX.Element, JSX.Element> =>
-//     pipe(
-//       fa,
-//       IO.map((sw) =>
-//         sw.error
-//           ? E.left(React.createElement('div', null, `Error: ${sw.error}`))
-//           : sw.data
-//           ? E.right(React.createElement(Component, { ...props, data: sw.data }))
-//           : E.left(React.createElement('div', null, 'loading...'))
-//       )
-//     );
