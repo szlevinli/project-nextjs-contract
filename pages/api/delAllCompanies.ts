9846a2b1d0b6f8b818a0c6947fedef45dd150d5c
levin li <3259963@qq.com>
2021-05-30 09:36:23 +0800

    保护 API

--- pages/api/delAllCompanies.ts
+++ pages/api/delAllCompanies.ts
@@ -1,9 +1,12 @@
 import { Company } from '../../lib/sqlite/models';
 import { NextApiHandler } from 'next';
+import { withApiAuthRequired, getSession } from '@auth0/nextjs-auth0';
 
-const handler: NextApiHandler = async (_, res) => {
+const handler: NextApiHandler = async (req, res) => {
+  const { user } = getSession(req, res);
+  console.log(`[API:delAllCompanies]: user=${JSON.stringify(user)}`);
   const deletedNum = await Company.destroy({ truncate: true });
   res.status(200).json({ deleted_number: deletedNum });
 };
 
-export default handler;
+export default withApiAuthRequired(handler);
