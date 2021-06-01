5977dfa3c383e3f19f4f9dd48b2abfacf9eb2290
levin li <3259963@qq.com>
2021-06-01 22:09:26 +0800

    优化实现. 增加认证

--- pages/api/addCompany.ts
+++ pages/api/addCompany.ts
@@ -1,10 +1,11 @@
-import { Company } from '../../lib/sqlite/models';
+import { Company, CompanyFields } from '../../lib/sqlite/models';
 import { NextApiHandler } from 'next';
+import { withApiAuthRequired } from '@auth0/nextjs-auth0';
 
 const handler: NextApiHandler = async (req, res) => {
   const income = req.body;
-  const record = await Company.create(income);
+  const record = (await Company.create(income)).toJSON() as CompanyFields;
   res.status(200).json(record);
 };
 
-export default handler;
+export default withApiAuthRequired(handler);
