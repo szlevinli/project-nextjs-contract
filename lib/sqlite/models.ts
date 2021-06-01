b6fa2b2c1b5442b3aad8171aa1ead1a48ca5c8ce
levin li <3259963@qq.com>
2021-06-01 15:10:45 +0800

    添加类型约束, 完善类型规约

--- lib/sqlite/models.ts
+++ lib/sqlite/models.ts
@@ -1,67 +1,57 @@
+import { pipe } from 'fp-ts/function';
 import {
   DataTypes,
+  Model,
   ModelAttributeColumnOptions,
   ModelAttributes,
   ModelOptions,
 } from 'sequelize';
 import db from './db';
-import { pipe } from 'fp-ts/function';
 
 const createTable =
-  (tableName: string) =>
-  (tableOptions: ModelOptions) =>
-  (fields: ModelAttributes) =>
-    db.define(tableName, fields, tableOptions);
+  <T>(tableName: string) =>
+  (tableOptions: ModelOptions<Model<T>>) =>
+  (fields: ModelAttributes<Model<T>>) =>
+    db.define<Model<T>>(tableName, fields, tableOptions);
 
 const addField =
-  (fieldName: string) =>
-  (fieldOptions: ModelAttributeColumnOptions) =>
-  (fields: ModelAttributes | {}): ModelAttributes => ({
+  <T>(fieldName: keyof T) =>
+  (fieldOptions: ModelAttributeColumnOptions<Model<T>>) =>
+  (fields: ModelAttributes<Model<T>> | {}): ModelAttributes<Model<T>> => ({
     ...fields,
     [fieldName]: fieldOptions,
   });
 
 //
+// Common
+//
+
+export type CommonFields = {
+  id: number;
+  createdAt: Date;
+  updatedAt: Date;
+};
+
+//
 // Model: Company
 //
 
-const companyTable = createTable('Company')({ modelName: 'Companies' });
+export type CompanyCreationFields = {
+  name: string;
+  abbr: string;
+};
 
-const nameField = addField('name')({ type: DataTypes.STRING });
-const abbrField = addField('abbr')({ type: DataTypes.STRING });
+export type CompanyFields = CommonFields & CompanyCreationFields;
+
+const companyTable = createTable<CompanyCreationFields>('Company')({
+  modelName: 'Companies',
+});
+
+const nameField = addField<CompanyCreationFields>('name')({
+  type: DataTypes.STRING,
+});
+const abbrField = addField<CompanyCreationFields>('abbr')({
+  type: DataTypes.STRING,
+});
 
 export const Company = pipe({}, nameField, abbrField, companyTable);
-
-//
-// Create DB
-//
-
-// (async () => {
-//   await db.sync();
-//   console.log(`All models were synchronized successfully.`);
-// })();
-
-// TODO: Delete
-// (async () => {
-//   const all = await Company.findAll();
-//   console.log(`Company records:`);
-//   all.forEach((v) => console.log(v.toJSON()));
-
-//   const a = await Company.create({
-//     name: '招商蛇口',
-//     abbr: '招蛇',
-//   });
-//   console.log(`add new record ${a.toJSON()}`);
-// })();
-
-// const Company = db.define(
-//   'Company',
-//   {
-//     name: {
-//       type: DataTypes.STRING,
-//     },
-//   },
-//   {
-//     modelName: 'Companies',
-//   }
-// );
