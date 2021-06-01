58e687a2c1fc780784e0d07699f6c427843f586a
levin li <3259963@qq.com>
2021-06-01 22:08:34 +0800

    添加 `SnackbarProvider`

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,14 @@
 import '../styles/globals.css';
 import { UserProvider } from '@auth0/nextjs-auth0';
+import { SnackbarProvider } from 'notistack';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <UserProvider>
-      <Component {...pageProps} />
-    </UserProvider>
+    <SnackbarProvider maxSnack={3}>
+      <UserProvider>
+        <Component {...pageProps} />
+      </UserProvider>
+    </SnackbarProvider>
   );
 }
 
