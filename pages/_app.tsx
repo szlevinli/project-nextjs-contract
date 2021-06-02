d7d2b48936d60d3c6a1cc82dc701227e553bc1c9
levin li <3259963@qq.com>
2021-06-02 17:14:10 +0800

    add theme

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,20 @@
 import '../styles/globals.css';
 import { UserProvider } from '@auth0/nextjs-auth0';
 import { SnackbarProvider } from 'notistack';
+import { ThemeProvider } from '@material-ui/core/styles';
+import CssBaseline from '@material-ui/core/CssBaseline';
+import { theme } from '../lib/ui/theme';
 
 function MyApp({ Component, pageProps }) {
   return (
-    <SnackbarProvider maxSnack={3}>
-      <UserProvider>
-        <Component {...pageProps} />
-      </UserProvider>
-    </SnackbarProvider>
+    <ThemeProvider theme={theme}>
+      <SnackbarProvider maxSnack={3}>
+        <UserProvider>
+          <CssBaseline />
+          <Component {...pageProps} />
+        </UserProvider>
+      </SnackbarProvider>
+    </ThemeProvider>
   );
 }
 
