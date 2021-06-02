f6b36260a786f12e946f9c81d9c7b2bf3c3a6c17
levin li <3259963@qq.com>
2021-06-02 17:15:29 +0800

    删除过时的文件

--- components/withHandler.tsx
+++
@@ -1,8 +0,0 @@
-const withHandler =
-  <Data, Props>(handlerName: string) =>
-  (handler: (data: Data) => Promise<unknown>) =>
-  (Component: React.FC<Props>) =>
-  (props: Props) =>
-    <Component {...props} {...{ [handlerName]: handler }} />;
-
-export default withHandler;
