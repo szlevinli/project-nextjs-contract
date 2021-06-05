af488eddedd676e2edfff7a50c5dcb0b133f4cb8
levin li <3259963@qq.com>
2021-06-05 17:37:26 +0800

    使用fp-ts重构. 并添加异常处理

--- pages/api/addCompany.ts
+++ pages/api/addCompany.ts
@@ -1,11 +1,39 @@
-import { Company, CompanyFields } from '../../lib/sqlite/models';
-import { NextApiHandler } from 'next';
 import { withApiAuthRequired } from '@auth0/nextjs-auth0';
+import { fold, mapLeft as eitherMapLeft } from 'fp-ts/Either';
+import { pipe } from 'fp-ts/function';
+import { chain, fromEither, tryCatch } from 'fp-ts/TaskEither';
+import reporter from 'io-ts-reporters';
+import { NextApiHandler } from 'next';
+import { join } from 'ramda';
+import { Company, CompanyCreationFields } from '../../lib/sqlite/models';
+import { AddCompanyValidator } from '../../lib/utils/validator';
+
+const createCompany = (company: CompanyCreationFields) =>
+  tryCatch(
+    () => Company.create(company),
+    (reason) => new Error(String(reason))
+  );
 
 const handler: NextApiHandler = async (req, res) => {
-  const income = req.body;
-  const record = (await Company.create(income)).toJSON() as CompanyFields;
-  res.status(200).json(record);
+  const validatedResult = AddCompanyValidator.decode(req.body);
+  const task = pipe(
+    validatedResult,
+    eitherMapLeft((_) =>
+      pipe(validatedResult, reporter.report, join('\n'), Error)
+    ),
+    fromEither,
+    chain((company) => createCompany(company))
+  );
+
+  const result = await task();
+
+  pipe(
+    result,
+    fold(
+      (e) => res.status(400).json(e),
+      (d) => res.status(200).json(d)
+    )
+  );
 };
 
 export default withApiAuthRequired(handler);
