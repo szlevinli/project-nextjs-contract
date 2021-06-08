d484545b28ce807aa41bcfd0a144b5a1af93c589
levin li <3259963@qq.com>
2021-06-08 11:56:33 +0800

    捕获更详细的服务的返回错误

--- lib/utils/helper.ts
+++ lib/utils/helper.ts
@@ -1,10 +1,21 @@
-import axios from 'axios';
-import { tryCatch } from 'fp-ts/TaskEither';
+import axios, { AxiosResponse } from 'axios';
+import { tryCatch, chain, fromPredicate } from 'fp-ts/TaskEither';
+import { pipe } from 'fp-ts/function';
 
 export const callApi =
   <Body, T>(url: string) =>
   (body?: Body) =>
-    tryCatch(
-      () => axios.post<T>(url, body),
-      (err) => new Error(String(err))
+    pipe(
+      tryCatch(
+        () => axios.post<T>(url, body),
+        (err) => new Error(String(err))
+      ),
+      // 对于服务器返回的状态码大于等于 290 的当做错误处理
+      // 目的是捕捉服务器更详尽的错误
+      chain(
+        fromPredicate(
+          (d: AxiosResponse<T>) => d.status >= 200 && d.status < 290,
+          (d: AxiosResponse<T>) => new Error(String(d.data))
+        )
+      )
     );
