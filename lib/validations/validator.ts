0a5488d2654e9852e9a8573bc3c339c26bc623f8
levin li <3259963@qq.com>
2021-06-11 20:16:05 +0800

    添加类型约束

--- lib/validations/validator.ts
+++ lib/validations/validator.ts
@@ -1,5 +1,6 @@
 import * as io from 'io-ts';
 import * as t from 'io-ts-types';
+import { CommonFields, CompanyCreateFields } from '../sqlite/models';
 
 export const ActivityValidator = io.type({
   startTime: io.string,
@@ -16,23 +17,27 @@ export type ActivityArray = io.TypeOf<typeof ActivityArrayValidator>;
 // Common Fields
 //
 
-const CommonFields = {
+const CommonFields_: { [key in keyof CommonFields] } = {
   id: io.number,
   createdAt: t.DateFromISOString,
   updatedAt: t.DateFromISOString,
+  deletedAt: io.union([t.DateFromISOString, io.nullType]),
 };
 
 //
 // Company
 //
 
-const CompanyFields = {
+const CompanyCreateFields_: { [key in keyof CompanyCreateFields] } = {
   name: io.string,
   abbr: io.string,
 };
 
-export const CompanyValidator = io.type({ ...CommonFields, ...CompanyFields });
+export const CompanyValidator = io.type({
+  ...CommonFields_,
+  ...CompanyCreateFields_,
+});
 export const CompaniesValidator = io.array(CompanyValidator);
 
-export const AddCompanyValidator = io.type(CompanyFields);
+export const AddCompanyValidator = io.type(CompanyCreateFields_);
 export const AddCompaniesValidator = io.array(AddCompanyValidator);
