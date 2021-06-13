b894e4570d02318a2cba3059bec2b376e2f25ac5
levin li <3259963@qq.com>
2021-06-13 17:34:35 +0800

    Modify some variable names to make them more meaningful

--- pages/api/addCompany.ts
+++ pages/api/addCompany.ts
@@ -3,12 +3,12 @@ import * as E from 'fp-ts/Either';
 import { pipe } from 'fp-ts/function';
 import * as TE from 'fp-ts/TaskEither';
 import { Validation } from 'io-ts';
-import reporter from 'io-ts-reporters';
+import { formatValidationErrors } from 'io-ts-reporters';
 import { NextApiHandler } from 'next';
 import { join } from 'ramda';
 import { Company, CompanyCreateFields } from '../../lib/sqlite/models';
 import { validateCompany } from '../../lib/validations/companyValidation';
-import { AddCompanyValidator } from '../../lib/validations/validator';
+import { CreateCompanyValidator } from '../../lib/validations/validator';
 
 const createCompany = (company: CompanyCreateFields) =>
   TE.tryCatch(
@@ -21,7 +21,7 @@ const handler: NextApiHandler = async (req, res) => {
   // 这里使用 `io-ts` 进行静态类型验证
   // Validation<CompanyCreationFields> 实际上等同于 Either<Errors, CompanyCreationFields>
   const validatedResult: Validation<CompanyCreateFields> =
-    AddCompanyValidator.decode(req.body);
+    CreateCompanyValidator.decode(req.body);
 
   // 以下代码通过 `fp-ts/TaskEither` 实现如下的目标:
   //   - 如果客户端传递过来的数据通过静态类型验证, 则调用创建 company api
@@ -32,7 +32,7 @@ const handler: NextApiHandler = async (req, res) => {
 
     // Either<Error, CompanyCreationFields>
     // 将 Errors 转换成 Error 便于后续 composition
-    E.mapLeft((_) => pipe(validatedResult, reporter.report, join('\n'), Error)),
+    E.mapLeft((e) => pipe(e, formatValidationErrors, join('\n'), Error)),
 
     // 验证传入的 company 的值
     E.chain(validateCompany),
