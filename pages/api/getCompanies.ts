a9aefc5b76ab446d18021514d7bc55a0a2a81871
levin li <3259963@qq.com>
2021-06-15 15:41:21 +0800

    Refactor with fp-ts

--- pages/api/getCompanies.ts
+++ pages/api/getCompanies.ts
@@ -1,12 +1,24 @@
+import { withApiAuthRequired } from '@auth0/nextjs-auth0';
+import { pipe } from 'fp-ts/function';
+import { tryCatch, fold } from 'fp-ts/TaskEither';
 import { NextApiHandler } from 'next';
 import { Company, CompanyAllFields } from '../../lib/sqlite/models';
 
-const handler: NextApiHandler = async (_, res) => {
-  const companies = (await Company.findAll()).map(
-    (v) => v.toJSON() as CompanyAllFields
+const queryCompany = () =>
+  tryCatch(
+    () => Company.findAll(),
+    (reason) => new Error(String(reason))
   );
 
-  res.status(200).json(companies);
+const queryCompanyHandler: NextApiHandler = async (_, res) => {
+  await pipe(
+    queryCompany(),
+    fold(
+      (e) => async () => res.status(290).json(String(e)),
+      (d) => async () =>
+        res.status(200).json(d.map((v) => v.toJSON() as CompanyAllFields))
+    )
+  )();
 };
 
-export default handler;
+export default withApiAuthRequired(queryCompanyHandler);
