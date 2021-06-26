ed01d4e64cfaba2e7e3550af9e555eb4206de9fb
levin li <3259963@qq.com>
2021-06-26 15:55:50 +0800

    调用 `deleteRecord` 替代自编码

--- pages/api/delCompany.ts
+++ pages/api/delCompany.ts
@@ -1,35 +1,8 @@
 import { withApiAuthRequired } from '@auth0/nextjs-auth0';
-import * as E from 'fp-ts/Either';
-import { pipe } from 'fp-ts/function';
-import * as TE from 'fp-ts/TaskEither';
-import { formatValidationErrors } from 'io-ts-reporters';
-import { NextApiHandler } from 'next';
-import { join } from 'ramda';
-import { Company, PkFields } from '../../lib/sqlite/models';
+import { Company } from '../../lib/sqlite/models';
+import { deleteRecord } from '../../lib/utils/deleteRecord';
 import { DeleteCompanyValidator } from '../../lib/validations/validator';
 
-const deleteCompany = ({ id }: PkFields) =>
-  TE.tryCatch(
-    () =>
-      Company.destroy({
-        where: {
-          id,
-        },
-      }),
-    (reason) => new Error(String(reason))
-  );
-
-const deleteCompanyHandler: NextApiHandler = async (req, res) => {
-  await pipe(
-    DeleteCompanyValidator.decode(req.body),
-    E.mapLeft((e) => pipe(e, formatValidationErrors, join('\n'), Error)),
-    TE.fromEither,
-    TE.chain((d) => deleteCompany(d)),
-    TE.fold(
-      (e) => async () => res.status(290).json(String(e)),
-      (d) => async () => res.status(200).json(d)
-    )
-  )();
-};
+const deleteCompanyHandler = deleteRecord(Company)(DeleteCompanyValidator);
 
 export default withApiAuthRequired(deleteCompanyHandler);
