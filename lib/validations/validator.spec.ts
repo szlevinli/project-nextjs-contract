254397967b64a297a8ce11892f71ae7f30fa2636
levin li <3259963@qq.com>
2021-07-07 14:00:15 +0800

    add unit test

--- lib/validations/validator.spec.ts
+++ lib/validations/validator.spec.ts
@@ -1,7 +1,7 @@
-import { DeleteCompanyValidator } from './validator';
 import { isLeft, isRight } from 'fp-ts/Either';
+import { DeleteCompanyValidator, UpdateCompanyValidator } from './validator';
 
-it('should be Right', () => {
+it('[DeleteCompanyValidator]: should be Right', () => {
   const result = DeleteCompanyValidator.decode({
     where: {
       id: 1,
@@ -10,12 +10,44 @@ it('should be Right', () => {
   expect(isRight(result)).toBeTruthy();
 });
 
-it('should be Left', () => {
+it('[DeleteCompanyValidator]: should be Left', () => {
   const result = DeleteCompanyValidator.decode({
     where: {
       id: 1,
       name: 15,
     },
   });
+  expect(isLeft(result)).toBeTruthy();
+});
+
+it('[UpdateCompanyValidator]: should be Right', () => {
+  const body = {
+    values: {
+      name: 'name1',
+      abbr: 'abbr1',
+    },
+    options: {
+      where: {
+        id: 1,
+      },
+    },
+  };
+  const result = UpdateCompanyValidator.decode(body);
+  expect(isRight(result)).toBeTruthy();
+});
+
+it('[UpdateCompanyValidator]: should be Left', () => {
+  const body = {
+    values: {
+      name: 'name1',
+      abbr: 15,
+    },
+    options: {
+      where: {
+        id: 'id01',
+      },
+    },
+  };
+  const result = UpdateCompanyValidator.decode(body);
   expect(isLeft(result)).toBeTruthy();
 });
